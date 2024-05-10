e21527d4d0eaf8a10fe5d7d992b76b56e92a3797
Oleg <146124252+olegusanet87@users.noreply.github.com>
2024-05-10 22:15:46 +0300

    lesson 5 complete

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from '../pages/NotFound';
+import { AppBar } from './AppBar';
+import css from './App.module.css';
+
+const Home = lazy(() => import('../pages/Home'));
+const About = lazy(() => import('../pages/About'));
+const ProductDetails = lazy(() => import('../pages/ProductDetails'));
+const Products = lazy(() => import('../pages/Products'));
+const Mission = lazy(() => import('./Mission'));
+const Team = lazy(() => import('./Team'));
+const Reviews = lazy(() => import('./Reviews'));
+
+export default function App () {
+  return (
+    <div className={css.container}>
+      <AppBar />
+
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}>
+            <Route path="mission" element={<Mission />} />
+            <Route path="team" element={<Team />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,19 @@
+import { Link, useLocation } from 'react-router-dom';
+import css from './ProductList.module.css';
+
+export const ProductList = ({ products }) => {
+  const location = useLocation();
+
+  return (
+    <div className={css.container}>
+      {products.map((product) => (
+        <div key={product.id} className={css.cardWrapper}>
+          <Link to={`${product.id}`} state={location}>
+            <img src="https://via.placeholder.com/200x100" alt="" />
+            <h3 className={css.productName}>{product.name}</h3>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
